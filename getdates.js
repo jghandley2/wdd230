45051a0b0396b316f86704f6c3597e6c3a2d64e8
jghandley2 <167990019+jghandley2@users.noreply.github.com>
2024-09-21 20:39:45 +0200

    Add files via upload

---
+++ getdates.js
@@ -0,0 +1,6 @@
+// Populate the current year in the footer
+document.getElementById('currentYear').textContent = new Date().getFullYear();
+
+// Populate the last modified date in the footer
+document.getElementById('lastModified').textContent = "Last Modified: " + document.lastModified;
+
